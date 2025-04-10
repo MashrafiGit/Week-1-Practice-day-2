92fa7b9252bd91d32f8b828d3242c1283de03fee
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-10 15:57:59 +0600

    sum of tow numbers

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## #Week 1 - Practice day 2
 
-1. I Love Practice
+1. Print I Love Practice.
 2. Sum of Two Numbers
 3. Variables
 4. N Times
---
+++ sum_of_tow_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    // get value from user
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    // print the summation
+    printf("%d", A + B);
+
+    return 0;
+}
