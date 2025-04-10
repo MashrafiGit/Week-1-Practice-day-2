540881e3890560e2c602cdda47f1300bd88fda61
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-10 16:13:49 +0600

    print N times i love practice

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## #Week 1 - Practice day 2
 
 1. Print I Love Practice.
-2. Sum of Two Numbers
-3. Variables
-4. N Times
-5. Divisible By 5 or Not
+2. Sum of Two Numbers.
+3. Variables.
+4. Print N Times.
+5. Divisible By 5 or Not.
---
+++ print_N_times.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    // get value form user
+    int N;
+    scanf("%d", &N);
+
+    // print N times I Love Practice
+    for( N; N>0; N -=1){
+        printf("I Love Practice\n");
+    }
+
+    return 0;
+}
